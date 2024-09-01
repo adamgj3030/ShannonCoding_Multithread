afda2699f507f81ad6dd8f2173979f7dc8c1e1e2
adamgj3030 <adamgj3030@gmail.com>
2024-08-31 19:46:24 -0500

    finished algorithm, W/O output structure

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <map>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
@@ -11,6 +12,29 @@ bool compare(pair<pair<char, int>, string> a, pair<pair<char, int>, string> b) {
         return a.first.second > b.first.second;
     }
     return a.first.first > b.first.first;
+}
+
+string decimalToBinary(float decimal, int precision) {
+    string binary = "";
+    while (decimal > 0 && precision > 0) {
+        double temp = decimal * 2;
+        if (temp >= 1) {
+            binary += "1";
+            decimal = temp - 1;
+        }
+        else {
+            binary += "0";
+            decimal = temp;
+        }
+        --precision;
+    }
+
+    while (precision > 0) {
+        --precision;
+        binary += "0";
+    }
+
+    return binary;
 }
 
 int main() {
@@ -39,6 +63,27 @@ int main() {
     for (auto temp : charCodeVec) {
         cout << temp.first.first << " , " << temp.first.second << " , " << temp.second << endl;
     }
+
+    float cumulativeProbability = 0;
+
+    for (auto& temp : charCodeVec) {
+        cout << "Frequency: " << temp.first.second << endl;
+
+        float probability = ((float)temp.first.second/len);
+        cout << "Probability: " << probability << endl;
+
+        int precision = ceil(log2(1/probability));
+        cout << "Precision: " << precision << endl;
+        cout << "cumulativeProbability: " << cumulativeProbability << endl;
+
+        string ShannonCode = decimalToBinary(cumulativeProbability, precision);
+        cout << "ShannonCode: " << ShannonCode << endl;
+
+        temp.second = ShannonCode;
+
+        cumulativeProbability += probability;
+    }
+
 
     return 0;
 }
