39ceeb22a08f6f9f197a01b9568062566cc75564
adamgj3030 <adamgj3030@gmail.com>
2024-09-10 15:38:43 -0500

    Added Struct CharFreq and the compare for sorting CharFreq

--- main2.cpp
+++ main2.cpp
@@ -7,13 +7,28 @@
 
 using namespace std;
 
-struct temp {
-
+// Struct to store character, frequency and finished binary code
+struct CharFreq {
+    char character;
+    int freq;
+    string code;
 };
+
+// Used for sorting function, Compares Frequency first then if equal, compares character 
+bool compare(CharFreq a, CharFreq b) {
+    
+    if (a.freq != b.freq) {
+        // if a.freq > b.freq, return true
+        return a.freq > b.freq;
+    }
+
+    // if a.freq = b.freq, return the character with the higher value
+    return a.character > b.character;
+}
 
 void shannonCode(string line) 
 {
-    
+
 
 }
 
