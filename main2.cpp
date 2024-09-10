cabb34204eb32068a705b184a78818b8e0291b04
adamgj3030 <adamgj3030@gmail.com>
2024-09-10 15:27:21 -0500

    started refined shannonCoding algorithm

---
+++ main2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+struct temp {
+
+};
+
+void shannonCode(string line) 
+{
+    
+
+}
+
+int main() 
+{
+    string line;
+    vector<string> input;
+    while(getline(cin, line)) {
+        input.push_back(line);
+    }
+
+    return 0;
+}
