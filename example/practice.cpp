445901a8bbe6af4cec8d2a934ab6f3cb4668b10f
adamgj3030 <adamgj3030@gmail.com>
2024-09-11 16:20:55 -0500

    Finished practice of pthread_create() and pthread_join()

--- example/practice.cpp
+++ example/practice.cpp
@@ -10,23 +10,38 @@ struct arguments
 };
 
 // Write the child thread function here
+void *dec2bin(void *void_ptr) 
+{
+    arguments *ptr = (arguments*) void_ptr;
+    int value = ptr->value;
+    while (value > 0)
+    {
+        int digit = value % 2;
+        ptr->binCode = std::to_string(digit) + ptr->binCode;
+        value = value / 2;
+    }
+
+    return nullptr;
+} 
 
 int main()
 {
     std::vector<arguments> arg;
+
     arguments tempHolder;
     tempHolder.binCode = "";
 	while (std::cin >> tempHolder.value)
     {
         arg.push_back(tempHolder);
     }
+
     int nValues = arg.size();
  	pthread_t *tid = new pthread_t[nValues];
         
 	for(int i = 0 ; i < nValues; i++)
 	{
         
-		if(/*Call pthread_create here*/ != 0) 
+		if(pthread_create(&tid[i], nullptr, dec2bin, (void *) &arg.at(i)) != 0) 
 		{
 			std::cerr << "Error creating thread" << std::endl;
 			return 1;
@@ -36,6 +51,10 @@ int main()
 	
 	// Wait for the other threads to finish.
     // Call pthread_join here
+    for (int i = 0;i < nValues; i++)
+    {
+        pthread_join(tid[i], nullptr);
+    }
 
     
     for (int i = 0; i < nValues; i++)
