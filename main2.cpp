e257b5ef2cb62328f8a458ceb544fe78fdc2d3ec
adamgj3030 <adamgj3030@gmail.com>
2024-09-11 17:23:45 -0500

    Added multithreading part of shannonCode, still need to edit how it works now that pointers are involved

--- main2.cpp
+++ main2.cpp
@@ -1,44 +1,90 @@
 #include <iostream>
 #include <vector>
+#include <pthread.h>
 #include <string>
 #include <map>
 #include <algorithm>
 #include <cmath>
 
-using namespace std;
-
-// Struct to store character, frequency and finished binary code
+// Struct to store line, character, frequency and finished binary code
 struct CharFreq {
-    char character;
-    int freq;
-    string code;
+    std::string line;
+    // Vector to store characters and their frequencies of said line
+    std::vector<std::pair<char,int>> charFreqVec;
+    std::string code;
 };
 
 // Used for sorting function, Compares Frequency first then if equal, compares character 
 bool compare(CharFreq a, CharFreq b) {
     
-    if (a.freq != b.freq) {
+    //if (a.freq != b.freq) {
         // if a.freq > b.freq, return true
-        return a.freq > b.freq;
+    //    return a.freq > b.freq;
+    //}
+
+    //// if a.freq = b.freq, return the character with the higher value
+    //return a.character > b.character;
+}
+
+
+void *shannonCode(void *void_ptr) 
+{
+    // change the void_ptr into a CharFreq struct ptr
+    CharFreq *charFreq_ptr = (CharFreq*) void_ptr;
+    
+    // create a mapping to store each unique character and count their appearances 
+    std::map<char, int> charCountMap;
+
+    // iterate through each character for the given line
+    // NOTE: COULD skip the mapping part and do this in the struct 
+    int len = (charFreq_ptr->line).length();
+    for (int i = 0; i < len; i++) 
+    {
+        // Stores unqiue characters and their associated frequencies 
+        ++charCountMap[charFreq_ptr->line[i]];
     }
 
-    // if a.freq = b.freq, return the character with the higher value
-    return a.character > b.character;
+    // Once the mapping is done, the frequency part of the CharFreq strut is updated 
+    for (const auto& charCount: charCountMap)
+    {
+        
+    }
+
+    return nullptr;
 }
 
-void shannonCode(string line) 
-{
-
-
-}
 
 int main() 
 {
-    string line;
-    vector<string> input;
-    while(getline(cin, line)) {
-        input.push_back(line);
+    // store each line into a different vector of struct CharFreq
+    CharFreq temp;
+    std::vector<CharFreq> charFreq;
+    while(std::getline(std::cin, temp.line)) {
+        charFreq.push_back(temp);
     }
+
+    int charFreqSize = charFreq.size();
+
+    // store the thread id in tid
+    pthread_t *tid = new pthread_t[charFreqSize];
+
+    // iterate through the vector
+    for (int i = 0; i < charFreqSize; i++)
+    {
+        // create a thread for each CharFreq struct of the vector, and if an error is thrown return 1;
+        if(pthread_create(&tid[i], nullptr, shannonCode, (void *) &charFreq[i]))
+        {
+            std::cerr << "Error creating thread" << std::endl;
+			return 1;
+        }
+    }
+
+    // join threads back together when they are finished
+    for (int i = 0; i < charFreqSize; i++)
+    {
+        pthread_join(tid[i], nullptr);
+    }
+    
 
     return 0;
 }
